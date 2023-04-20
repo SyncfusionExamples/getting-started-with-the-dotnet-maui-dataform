8e9c253a48ebec2625dd1cb451b86c21dcf323ee
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2023-04-20 15:46:42 +0530

    Adding MAUI DataForm sample
    Adding MAUI DataForm sample

---
+++ MauiProject/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiProject
+{
+    public class ViewModel
+    {
+        public EmployeeInfo Employee { get; set; }
+        public ViewModel() { 
+            this.Employee = new EmployeeInfo();
+        }
+    }
+}
